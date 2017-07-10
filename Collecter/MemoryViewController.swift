3c315b13f2cfbdacdeda6c0ff3f70b0bdf5c0e1b
Belal E <balolyman1234@gmail.com>
2017-07-09 22:35:52 -0400

    setup "add" page layout

---
+++ Collecter/MemoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MemoryViewController.swift
+//  Collecter
+//
+//  Created by mac pro on 7/9/17.
+//  Copyright © 2017 Elsiesy Industries. All rights reserved.
+//
+
+import UIKit
+
+class MemoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
