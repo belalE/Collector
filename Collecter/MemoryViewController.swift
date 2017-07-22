b047d8b15e735259d8a305c05d8f43030024ba1d
Belal E <balolyman1234@gmail.com>
2017-07-21 22:49:14 -0400

    We can create Memory Objects and pull them out of Core Data

--- Collecter/MemoryViewController.swift
+++ Collecter/MemoryViewController.swift
@@ -8,14 +8,49 @@
 
 import UIKit
 
-class MemoryViewController: UIViewController {
+class MemoryViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
     }
 
+    @IBOutlet weak var ImageView: UIImageView!
     
+    @IBOutlet weak var TitleTextField: UITextField!
+    
+    @IBAction func AddButton(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let memory = Memory(context: context)
+        memory.title = TitleTextField.text
+        memory.image = UIImagePNGRepresentation(ImageView.image!) as! NSData
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+        
+    }
+    @IBAction func PhotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        ImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func CameraTapped(_ sender: Any) {
+    }
 
 }
--- Collecter/ViewController.swift
+++ Collecter/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var TableView: UITableView!
+    
+    var memories : [Memory] = []
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            memories = try context.fetch(Memory.fetchRequest())
+            print(memories)
+            
+            TableView.reloadData()
+        } catch {
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let memory = memories[indexPath.row]
+        cell.textLabel?.text = memory.title
+        cell.imageView?.image = UIImage(data: memory.image as! Data)
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        TableView.dataSource = self
+        
+        TableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
